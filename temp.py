7e96bb3dd0ca45943c82a982773de7d7e34c14a6
darshanv223 <124893531+darshanv223@users.noreply.github.com>
2024-11-02 13:11:57 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -11,7 +11,7 @@ collection = db['videos']
 
 # Initialize the Instagram client
 client = Client()
-client.login('rooohaaan_07', 'Disha@007')
+client.login('diiishaaa_07', 'DarshanV@2234')
 
 def upload_reel(reel):
     url = reel['videoUrl']
